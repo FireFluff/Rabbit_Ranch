eae5f66ffb06528a3fee02c216bee12a17575b37
FireFluff <121639138+FireFluff@users.noreply.github.com>
2025-12-31 10:42:26 +0200

    Added new editor customization features and fixed task bar problem
    Added the naughty attributes for making the editor easier to read without knowing the code.
    Made it so that the game area starts above the taskbar but locked the game to only the windows OS to do so :(

--- Assets/Scripts/WindowControl.cs
+++ Assets/Scripts/WindowControl.cs
@@ -1,17 +1,23 @@
 using System.Drawing;
 using UnityEngine;
 using System.Windows.Forms;
+using TMPro;
 
 public class WindowControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private Vector2 WindowOriginPos;
+    private Vector2 WindowOriginPos;
     private int GameHeight;
+    /* More features can be added later
+    [Range(1,100)]
+    int GameScreenCoverage = 6;
+    [SerializeField] private TMP_Text debugText;
+    */
+    
     void Awake()
     {
-        /*System.Windows.Forms.Screen.GetWorkingArea();*/
-        GameHeight = UnityEngine.Screen.currentResolution.height / 6;
-        WindowOriginPos = new(0, UnityEngine.Screen.currentResolution.height - GameHeight);
+        var workingArea = System.Windows.Forms.Screen.GetWorkingArea(new Point(0, 0));
+        GameHeight = workingArea.Height / 6;
+        WindowOriginPos = new(0, workingArea.Height - GameHeight);
         BorderlessWindow.SetWindowPos(WindowOriginPos, UnityEngine.Screen.currentResolution.width, GameHeight);
     }
 
